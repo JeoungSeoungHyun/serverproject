2d15fd9d8a24eab979bb95eb8b18657ac70406c5
tjdgus7562 <tjdgus7562@naver.com>
2022-03-11 16:05:15 +0900

    2.리팩토링 및 UI수정

--- src/main/java/site/metatcoding/serverproject/domain/Hospital.java
+++ src/main/java/site/metatcoding/serverproject/domain/Hospital.java
@@ -14,18 +14,18 @@ import lombok.NoArgsConstructor;
 public class Hospital {
 
     @Id
-    private int id;
-    private String addr;
-    private Integer mgtStaDd;
-    private String pcrPsblYn;
-    private String ratPsblYn;
-    private Integer recuClCd;
-    private String sgguCdNm;
-    private String sidoCdNm;
-    private String yadmNm;
-    private String ykihoEnc;
-    private Double xposWgs84;
-    private Double yposWgs84;
-    private Integer xpos;
-    private Integer ypos;
+    private int id; // PK(Primary Key)
+    private String addr; // 주소
+    private Integer mgtStaDd; // 운영시작일자
+    private String pcrPsblYn; // PCR 가능여부
+    private String ratPsblYn; // RAT 가능여부
+    private Integer recuClCd; // 요양종별코드
+    private String sgguCdNm; // 시군구명
+    private String sidoCdNm; // 시도명
+    private String yadmNm; // 요양기관명
+    private String ykihoEnc; // 암호화된 요양기호
+    private Double xposWgs84; // 세계지구 x좌표
+    private Double yposWgs84; // 세계지구 y좌표
+    private Integer xpos; // x좌표
+    private Integer ypos; // y좌표
 }
--- src/main/java/site/metatcoding/serverproject/web/DownloadController.java
+++ src/main/java/site/metatcoding/serverproject/web/DownloadController.java
@@ -1,14 +1,14 @@
 package site.metatcoding.serverproject.web;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
@@ -19,16 +19,19 @@ import site.metatcoding.serverproject.domain.HospitalRepository;
 @Controller
 public class DownloadController {
 
+    // 어노테이션으로 자동으로 생성자를 통한 DI받기 위해 static 사용
     private final HospitalRepository hospitalRepository;
 
+    // 도메인 입력시 다운로드 할 수 있는 페이지로 이동시켜주는 메서드
     @GetMapping("/")
     public String move() {
         return "hospital/download";
     }
 
+    // 데이터를 다운받아 DB에 저장하는 메서드
     @GetMapping("/download")
-    public String download(Model model) {
-        // 1. URL로 다운로드
+    public String download() {
+        // 다운로드
         RestTemplate rt = new RestTemplate();
 
         StringBuffer sb = new StringBuffer();
@@ -36,7 +39,7 @@ public class DownloadController {
         sb.append("http://3.38.254.72:5000/api/hospital?");
         sb.append("sidoCdNm="); // 시도명
         sb.append("부산");
-        sb.append("&sgguCdNm="); // 군구명
+        sb.append("&sgguCdNm="); // 시군구명
         sb.append("부산사하구");
 
         // Item[] response = rt.getForEntity(sb.toString(), Item[].class);
@@ -44,11 +47,42 @@ public class DownloadController {
 
         List<Hospital> hospitals = Arrays.asList(response);
 
-        // 2. DB에 saveALL() + model에 담기
+        // DB에 saveALL() + model에 담기
         hospitalRepository.saveAll(hospitals);
+
+        return "/";
+    }
+
+    // DB로부터 데이터를 불러와 리스트페이지로 전달하는 메서드
+    @GetMapping("/list")
+    public String list(Model model) {
+
+        // DB로부터 데이터 받기
+        List<Hospital> hospitals = hospitalRepository.findAll();
+
+        // model에 담아 리스트페이지로 전달
         model.addAttribute("hospitals", hospitals);
-        // 3. 리턴
+
         return "hospital/list";
+    }
+
+    // PK(id)로 DB에서 SELECT하여 VIEW에 전달하는 메서드
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable int id, Model model) {
+
+        // DB로부터 SELECT
+        // null값도 담기 위해 Optional 사용
+        Optional<Hospital> opHopital = hospitalRepository.findById(id);
+
+        // 데이터가 있다면 model에 담아 view로 전달
+        if (opHopital.isPresent()) {
+            Hospital hospitalEntity = opHopital.get();
+            model.addAttribute("hospital", hospitalEntity);
+
+            return "hospital/detail";
+        } else {
+            return "hospital/list";
+        }
     }
 
 }
